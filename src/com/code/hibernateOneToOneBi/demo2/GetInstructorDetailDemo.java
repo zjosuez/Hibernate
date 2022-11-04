4fc9f55f34c61f9eb108f702be074809e5cd7d1e
Josué Teixeira <zjosuez@outlook.com>
2022-11-04 12:55:28 -0300

    Adding bidirectional connection

---
+++ src/com/code/hibernateOneToOneBi/demo2/GetInstructorDetailDemo.java
@@ -0,0 +1,46 @@
+package com.code.hibernateOneToOneBi.demo2;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.code.hibernateOneToOneBi.demo2.entity.Instructor;
+import com.code.hibernateOneToOneBi.demo2.entity.InstructorDetail;
+
+public class GetInstructorDetailDemo {
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                                    .configure("hibernate2.cfg.xml")
+                                    .addAnnotatedClass(Instructor.class)
+                                    .addAnnotatedClass(InstructorDetail.class)
+                                    .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+           
+            // start a transaction
+            session.beginTransaction();
+
+            // get the instructor detail object
+            int theId = 2;
+            InstructorDetail tempInstructorDetail =
+                                session.get(InstructorDetail.class, theId);
+            // print the instructor detail
+            System.out.println("tempInstructorDetail: " + tempInstructorDetail);
+            // print the associated instructor
+            System.out.println("the associated instructor: " + tempInstructorDetail.getInstructor());
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        }
+        finally {
+            factory.close();
+        }
+    }
+}

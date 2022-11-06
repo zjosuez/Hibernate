c5973cbc57f8e4f9b7ae2eda04d64d63057d670b
Josué Teixeira <zjosuez@outlook.com>
2022-11-06 15:41:35 -0300

    remove cascade method

--- src/com/code/hibernateOneToOneBi/demo2/DeleteInstructorDetailDemo.java
+++ src/com/code/hibernateOneToOneBi/demo2/DeleteInstructorDetailDemo.java
@@ -26,7 +26,7 @@ public class DeleteInstructorDetailDemo {
             session.beginTransaction();
 
             // get the instructor detail object
-            int theId = 2;
+            int theId = 3;
             InstructorDetail tempInstructorDetail =
                                 session.get(InstructorDetail.class, theId);
             // print the instructor detail
@@ -36,6 +36,12 @@ public class DeleteInstructorDetailDemo {
 
             // now let's delete the instructor detail
             System.out.println("Deleting tempInstructorDetail: " + tempInstructorDetail);
+
+            // remove the associated object reference
+            // break bi-directional link
+
+            tempInstructorDetail.getInstructor().setInstructorDetail(null);
+
             session.delete(tempInstructorDetail);
 
             // commit transaction

1cde646079e3a4037c6d5265d1bbd8706ca26d56
Josué Teixeira <zjosuez@outlook.com>
2022-11-07 15:25:06 -0300

    added new instructions for id

--- src/com/code/hibernateOneToMany/demo2/entity/Course.java
+++ src/com/code/hibernateOneToMany/demo2/entity/Course.java
@@ -1,6 +1,13 @@
 package com.code.hibernateOneToMany.demo2.entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -16,11 +23,16 @@ public class Course {
     // define toString
 
     // annotate fields
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
 
+    @Column(name = "title")
     private String title;
 
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name = "instructor_id")
     private Instructor instructor;
 
     public Course(String title) {
